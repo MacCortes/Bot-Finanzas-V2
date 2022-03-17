485809cda9e8b27ffd9154f0d0d152a4f6815f62
Heryc Cortés <heryccortes@gmail.com>
2022-03-16 19:02:40 -0600

    commenting and renaming of variables in bot.py

--- bot.py
+++ bot.py
@@ -4,24 +4,26 @@ import logging
 from telegram import Update
 from telegram.ext import Updater, CallbackContext, CommandHandler, MessageHandler, Filters
 
+# For logging how the bot it's doing
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+# Handler functions
 def start(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
 def echo(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
-# For logging how the bot it's doing
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+# Bot configuration
+upd = Updater(token=bot_token, use_context=True)
 
-updater = Updater(token=bot_token, use_context=True)
+disp = upd.dispatcher
 
-dispatcher = updater.dispatcher
+disp.add_handler(CommandHandler('start', start))
 
-dispatcher.add_handler(CommandHandler('start', start))
+disp.add_handler(MessageHandler(Filters.text & (~Filters.command), echo))
 
-dispatcher.add_handler(MessageHandler(Filters.text & (~Filters.command), echo))
-
-updater.start_polling()
+upd.start_polling()
 
 print('Hello')
 
